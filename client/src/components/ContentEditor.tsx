af4d8f299a3d1d972fcc864e33345a9c02015ce2
Prajwal Pawar <prajwalpawar2303@gmail.com>
2024-11-21 14:16:41 +0530

    add ContentEditor

---
+++ client/src/components/ContentEditor.tsx
@@ -0,0 +1,37 @@
+import { Dispatch, SetStateAction } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+interface ContentEditorProps {
+  content: string;
+  setContent: Dispatch<SetStateAction<string>>;
+}
+
+const ContentEditor = ({ content, setContent }: ContentEditorProps) => {
+  const quillModules = {
+    toolbar: [
+      [{ header: [1, 2, 3, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  return (
+    <ReactQuill
+      value={content}
+      onChange={setContent}
+      modules={quillModules}
+      placeholder="Write your article here ....."
+      className="h-64 sm:h-80 text-xs sm:text-base"
+    />
+  );
+};
+
+export default ContentEditor;
--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar";
 import Loader from "./Loader";
+import ContentEditor from "./ContentEditor";
 
-export { Navbar, Loader };
+export { Navbar, Loader, ContentEditor };
