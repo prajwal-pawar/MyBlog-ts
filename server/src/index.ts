33d9fb7121f3440563d18d6244e4bd8eaaf8888a
Prajwal Pawar <prajwalpawar2303@gmail.com>
2024-11-23 11:58:42 +0530

    add express.static

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,6 +12,7 @@ const PORT = env.port;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use("/", routes);
 
