568776affa4c2b9c44abbc9b3bebab004a479b08
Prajwal Pawar <prajwalpawar2303@gmail.com>
2024-11-27 21:37:24 +0530

    add comments routes

---
+++ server/src/routes/comment.routes.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+  createComment,
+  deleteComment,
+} from "../controllers/comment.controller";
+import authenticateToken from "../middlewares/auth";
+
+const router = express.Router();
+
+router.post("/create", authenticateToken, createComment);
+router.delete("/delete", authenticateToken, deleteComment);
+
+export default router;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import authRoutes from "./auth.routes";
 import articleRoutes from "./article.routes";
 import userRoutes from "./user.routes";
+import commentRoutes from "./comment.routes";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/article", articleRoutes);
 router.use("/user", userRoutes);
+router.use("/comment", commentRoutes);
 
 export default router;
